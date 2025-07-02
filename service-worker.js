78b09e629d42657d3b6fce38eab02cd6d5124408
laterrassedemontresor <Patrimoine.et.renovation37@gmail.com>
2025-07-03 00:46:49 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Xtof debut modif
-const CACHE_VERSION = '2025-07-02q'; // Incrémentez cette version à chaque modification
+const CACHE_VERSION = '2025-07-02r'; // Incrémentez cette version à chaque modification
 const CACHE_NAME = `my-app-cache-${CACHE_VERSION}`;
 // Xtof fin modif
 const urlsToCache = [
