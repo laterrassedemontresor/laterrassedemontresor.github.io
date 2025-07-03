cb8e9e1a041ca10bb956ac2b3a72f23d0dc8d173
laterrassedemontresor <Patrimoine.et.renovation37@gmail.com>
2025-07-03 13:18:49 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2025-07-03d'; // Version mise à jour
+const CACHE_VERSION = '2025-07-03e'; // Version mise à jour
 const CACHE_NAME = `montresor-gate-cache-${CACHE_VERSION}`;
 const ASSETS_TO_CACHE = [
   '/',
